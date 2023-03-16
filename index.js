428fa7bc3e60b367d0a9ea851685d5aa78c10b05
Grellheist <grellheist@tutanota.com>
2023-03-16 14:15:54 -0300

    Added some basic express shenanigans

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const express = require("express");
 const app = express();
+const http = require("http");
+const server = http.createServer(app);
 
 const PORT = 3000 || process.env.PORT;
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html")
+})
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+});
