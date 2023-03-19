39cc6fe8b919ea59b684f2cafadebc73ed35f077
grellheist <grellheist@tutanota.com>
2023-03-19 13:58:14 -0300

    Updated

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Real time chat
 
 This will be my first real project, and will be programmed using Javascript.
+
+For now this will be a very crude implementation but it is going to get more
+sophisticated over time.
--- index.js
+++ index.js
@@ -5,22 +5,28 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+// Uses port 3000 by default, otherwise uses the PORT environmental variable
 const PORT = 3000 || process.env.PORT;
 
+// Normal express stuff
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
 })
 
+// Some socket.io shenanigans, listens to connections and disconnections
+// Also broadcasts messages to everyone
 io.on('connection', (socket) => {
     socket.on("chat message", (msg) => {
-        console.log("message: " + msg);
+        io.emit("chat message", `ID ${socket.id} said: ${msg}`);
     });
-    console.log('a user connected');
+    console.log(`User with ID ${socket.id} has connected!`);
     socket.on('disconnect', () => {
-        console.log('user disconnected');
+        console.log(`User with ID ${socket.id} has disconnected!`);
     });
 });
 
-app.listen(PORT, () => {
+// Initializes the connection on port 3000
+// Notice that it is using the server.listen and not app.listen
+server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 });
