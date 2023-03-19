7e39397d2239a2a94bf1807c40d58929641aeaea
grellheist <grellheist@tutanota.com>
2023-03-19 13:35:04 -0300

    Updated

--- index.js
+++ index.js
@@ -2,13 +2,25 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 const PORT = 3000 || process.env.PORT;
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/index.html")
+    res.sendFile(__dirname + "/views/index.html");
 })
 
+io.on('connection', (socket) => {
+    socket.on("chat message", (msg) => {
+        console.log("message: " + msg);
+    });
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`)
+    console.log(`Listening on port ${PORT}`);
 });
